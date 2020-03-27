6b4eaf04c862d7b93c8438ff3d3ef33fe3689a59
Maxim Ziatdinov <ziatdinovmax@gmail.com>
2020-03-27 23:42:35 +0000

    Temporarily downgrade matplotlib to v3.1.1

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ if __name__ == "__main__":
         zip_safe=False,
         install_requires=[
             'numpy>=1.16.5',
-            'matplotlib>=3.1.1',
+            'matplotlib==3.1.1',
             'torch>=1.3.1',
             'pyro-ppl>=0.4.1',
             'gpytorch>=0.3.6',
