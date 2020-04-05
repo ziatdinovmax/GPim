3590967824f3867a0b070bc21acd627021649608
ziatdinovmax <ziatdinovmax@gmail.com>
2020-04-05 15:36:13 -0400

    Update __init__.py

--- gpim/__init__.py
+++ gpim/__init__.py
@@ -1,3 +1,5 @@
-from gpim import gpr, skgpr, gprutils
+from gpim import gprutils as utils
+from gpim import kernels
 from gpim.gpr import reconstructor
 from gpim.skgpr import skreconstructor
+from gpim.vgpr import vgpr
