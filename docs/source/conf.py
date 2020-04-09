3a68485e8b239bfc055c945e0f93f1d72b888521
Maxim Ziatdinov <34245227+ziatdinovmax@users.noreply.github.com>
2020-04-08 23:46:09 -0400

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -72,6 +72,9 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # The name of the Pygments (syntax highlighting) style to use.
 pygments_style = 'sphinx'
 
+# do not prepend module name to functions
+add_module_names = False
+
 
 # -- Options for HTML output -------------------------------------------------
 
