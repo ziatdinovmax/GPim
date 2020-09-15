d0a4acbf8a7a30c268390b55ac73aa3ccc21ef37
Maxim Ziatdinov <34245227+ziatdinovmax@users.noreply.github.com>
2020-09-15 03:07:26 -0400

    Update version

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 __author__ = "Maxim Ziatdinov"
 __copyright__ = "Copyright Maxim Ziatdinov (2020)"
-__version__ = "0.3.5"
+__version__ = "0.3.6"
 __maintainer__ = "Maxim Ziatdinov"
 __email__ = "maxim.ziatdinov@ai4microcopy.com"
 __date__ = "05/20/2020"
@@ -13,7 +13,7 @@ module_dir = os.path.dirname(os.path.abspath(__file__))
 if __name__ == "__main__":
     setup(
         name='gpim',
-        version='0.3.5',
+        version='0.3.6',
         description='Gaussian processes for image analysis',
         long_description=open(os.path.join(module_dir, 'README.md')).read(),
         long_description_content_type='text/markdown',
